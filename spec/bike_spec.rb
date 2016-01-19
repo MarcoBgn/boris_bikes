264c81ba8dba49fb67d0fe6c83e95a5dbdd3a7ac
Marco Bagnasco <admin@hecticmojo.com>
2016-01-19 17:11:01 +0000

    Adds .dock_bike method, storing bikes into an instance variable array (@bikes) accessible with .bikes (reader only)

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -4,4 +4,7 @@ describe Bike do
   it "responds to .working?" do
     expect(subject).to respond_to(:working?)
   end
+  it "Returns true when passed .working?" do
+    expect(subject.working?).to eq true
+  end
 end
--- spec/dockingStation_spec.rb
+++ spec/dockingStation_spec.rb
@@ -7,5 +7,19 @@ describe DockingStation do
   it "Responds to .release_bike" do
     expect(subject).to respond_to(:release_bike)
   end
+  it "Releases a new instance of Bike when .release_bike is called" do
+    expect(subject.release_bike).to_not eq nil 
+  end
+  it "Responds to .dock_bike" do
+    expect(subject).to respond_to(:dock_bike)
+  end
+  it "Can access the @bikes instance variable" do
+    expect(subject.bikes).to eq []
+  end
+  
+ it "Adds an instance of Bike to an instance of DockingStation using .dock_bike" do
+    expect(subject.dock_bike(subject.release_bike)).to eq subject.bikes
+  end
+  
   
 end
